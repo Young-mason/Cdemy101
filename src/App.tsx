9dd0bb7147c0d7eb0bd7f2e21fba03177fd6d435
Young-Mason <mason.yk3@gmail.com>
2021-03-12 02:01:06 +0900

    Set directories & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
+import ReactDOM from "react-dom";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/products" />
+        </Route>
+        <Route path="/products" component={Products} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Products.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+function Products() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="carousel"></div>
+      <h1>Products</h1>
+    </div>
+  );
+}
+
+export default Products;
