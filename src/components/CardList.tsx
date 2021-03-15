fc6eb18b58a8fa4606f2fe1c1f2c91283b57962b
Young-Mason <mason.yk3@gmail.com>
2021-03-15 12:45:15 +0900

    Add animations in Modal & CardList

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { productItems } from "../productItems";
 import Pagination from "./Pagination";
 import pointer from "../modules/pointer";
@@ -6,6 +6,7 @@ import Card from "./Card";
 import "../style/CardList.css";
 
 function CardList() {
+  const [sliding, setSliding] = useState("");
   /* 한 페이지당 5개씩 들어가도록 페이지를 계산합니다 */
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
@@ -14,7 +15,24 @@ function CardList() {
   const currentItems = productItems.slice(indexOfFirstItem, indexOfLastItem);
 
   /* 상품 페이지를 이동시킵니다 */
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber: number, direction: string) => {
+    setSliding(direction);
+    setTimeout(() => {
+      setCurrentPage(pageNumber);
+      setSliding("");
+    }, 200);
+  };
+
+  /* 페이지 이동시 슬라이딩 효과를 줍니다 */
+  const slideLeft = {
+    transition: "transform 0.3s ease-in-out",
+    transform: "translateX(-90%)",
+  };
+
+  const slideRight = {
+    transition: "transform 0.3s ease-in-out",
+    transform: "translateX(90%)",
+  };
 
   /* 상품데이터를 Score 기준으로 내림차순 정렬합니다 */
   productItems.sort((a, b) => b.score - a.score);
@@ -22,7 +40,10 @@ function CardList() {
   return (
     <>
       {/* 현재 페이지에 나타날 데이터를 Card 컴포넌트와 매핑 */}
-      <div className="card-list">
+      <div
+        className="card-list"
+        style={sliding ? (sliding === "left" ? slideLeft : slideRight) : {}}
+      >
         {currentItems.map((item) => {
           const { id, title, coverImage, price } = item;
           return (
